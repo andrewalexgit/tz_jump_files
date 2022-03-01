3caeaa11e6e86324d128406fece456f14f942949
Andrew Campagna <40666746+andrewalexgit@users.noreply.github.com>
2022-03-01 09:19:36 -0600

    Update epel.sh

--- epel.sh
+++ epel.sh
@@ -1,5 +1,1 @@
-subscription-manager repos --enable rhel-*-optional-rpms \
-                           --enable rhel-*-extras-rpms \
-                           --enable rhel-ha-for-rhel-*-server-rpms
-yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm;
-yum install epel-release -y;
+subscription-manager repos --enable=rhel-7-server-extras-rpms
